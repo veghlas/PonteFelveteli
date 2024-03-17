aea4fde8ab51a88b2b8319efd69b49911697a9c9
VeghLaszlo <veghlas@gmail.com>
2024-03-17 12:48:33 +0100

    changelog-v1.0.xml added changelog-v2.0.xml added changelog-v3.0.xml added

---
+++ src/main/java/com/pontefelveteli/PonteFelveteliApplication.java
@@ -0,0 +1,13 @@
+package com.pontefelveteli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonteFelveteliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonteFelveteliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pontefelveteli/PonteFelveteliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontefelveteli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonteFelveteliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
