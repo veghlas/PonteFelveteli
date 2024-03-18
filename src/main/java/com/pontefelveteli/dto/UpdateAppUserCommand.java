2a9b3b246cf02b3d77a066c01c90adf7af066d2d
VeghLaszlo <veghlas@gmail.com>
2024-03-18 13:28:41 +0100

    valditation of appUserUpdate done

--- src/main/java/com/pontefelveteli/dto/UpdateAppUserCommand.java
+++ src/main/java/com/pontefelveteli/dto/UpdateAppUserCommand.java
@@ -5,12 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.util.List;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -18,7 +18,7 @@ import javax.validation.constraints.Pattern;
 public class UpdateAppUserCommand {
 
     @Pattern(regexp = "^[A-Za-záéiíoóöőuúüűÁÉIÍOÓÖŐUÚÜŰ]{2,20}",
-            message = "First name must be 2 to 20 characters without numeric digits.")
+            message = "Name must be 2 to 20 characters without numeric digits.")
     @NotBlank(message = "Name is required")
     private String name;
 
@@ -26,16 +26,25 @@ public class UpdateAppUserCommand {
     @Past(message = "Date of birth should be in the past")
     private LocalDate dateOfBirth;
 
-    @NotBlank(message = "Name of mother is required")
+    @Pattern(regexp = "^[A-Za-záéiíoóöőuúüűÁÉIÍOÓÖŐUÚÜŰ]{2,20}",
+            message = "Name of mother must be 2 to 20 characters without numeric digits.")
+    @NotBlank(message = "Name is required")
     private String nameOfMother;
 
     private UpdateAddressCommand updateAddressCommand;
 
+    @NotNull(message = "Cannot be null!")
     private Integer socialSecurityNumber;
 
+    @NotNull(message = "Cannot be null!")
     private Integer taxNumber;
 
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$",
+            message = "Incorrect email format!")
+    @NotBlank(message = "Cannot be blank!")
     private String email;
 
+    @NotBlank(message = "Phone number is required!")
+    @Pattern(regexp = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$", message = "Incorrect phone number format!")
     private List<String> phone_numbers;
 }
